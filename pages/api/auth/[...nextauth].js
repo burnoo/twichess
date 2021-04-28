f5cb856b560ef37e8156024fa76832ab00b4966e
burno <bruno.wieczorek@gmail.com>
2021-04-28 14:37:42 +0200

    Add prisma to project

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,9 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import Adapters from 'next-auth/adapters'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
 
 export default NextAuth({
   providers: [
@@ -8,4 +12,5 @@ export default NextAuth({
         clientSecret: process.env.TWITCH_CLIENT_SECRET
       }),
   ],
+  adapter: Adapters.Prisma.Adapter({ prisma }),
 })
