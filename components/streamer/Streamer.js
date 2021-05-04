4986d036615865a1cf2ec8ce874f6c9e20e46d79
burno <bruno.wieczorek@gmail.com>
2021-05-04 15:30:29 +0200

    Update table view

--- components/streamer/Streamer.js
+++ components/streamer/Streamer.js
@@ -1,5 +1,5 @@
 import Main from '../Main';
-import { Text, DataTable } from 'grommet';
+import { Box, Heading, Text, DataTable } from 'grommet';
 import { getRatingString } from '../../utils/string'
 import useSWR from 'swr'
 
@@ -16,39 +16,53 @@ function PlayersTable({ streamer }) {
   if (!data) return <Text>Loading...</Text>
 
   //TODO: use real data
-  const testData = [
-    {
-      index: 1,
-      name: "hubcio",
-      tempo: 'bullet',
-      rating: 2222
-    },
-    {
-      index: 1,
-      name: "crazypapa",
-      tempo: 'blitz',
-      rating: 2137
-    }
-  ]
+  const testData = Array.from({ length: 10 }, (_, i) => ({
+    index: i + 1,
+    name: `pypel${i + 1}`,
+    title: "GM",
+    tempo: i % 2 == 0 ? 'bullet' : 'blitz',
+    rating: 2837 - i * 100
+  }));
   const tableData = data.map((l, i) => ({
     index: i + 1,
+    title: l.title,
     name: l.lichessId,
     tempo: (l.topRating == l.rapidRating ? 'rapid' : l.topRating == l.blitzRating ? 'blitz' : 'bullet'),
-    rating: getRatingString(l.topRating)
+    rating: getRatingString(l.topRating),
   }))
   return <DataTable
+    background={{
+      body: ['light-1', 'light-3'],
+    }}
+    pad={{ horizontal: 'medium', vertical: 'small' }}
     columns={[
       {
         property: 'index',
       },
       {
+        property: 'title',
+        primary: true,
+      },
+      {
         property: 'name',
       },
       {
-        property: 'tempo',
-      },
-      {
         property: 'rating',
+        render: data => {
+          let iconLetter;
+          switch (data.tempo) {
+            case 'rapid':
+              iconLetter = 'T'
+              break;
+            case 'bullet':
+              iconLetter = ")"
+              break;
+            case 'blitz':
+              iconLetter = '#'
+              break;
+          }
+          return <Text data-lichess-icon={iconLetter} margin={{ left: "xlarge" }}>{data.rating}</Text>
+        }
       },
     ]}
     data={testData}
@@ -61,6 +75,9 @@ export default function Streamer({ isSignedIn, error, streamer }) {
   }
 
   return <Main isSignedIn={isSignedIn}>
-    <PlayersTable streamer={streamer} />
+    <Box direction="column" align="center">
+      <Heading size="small">{streamer}</Heading>
+      <PlayersTable streamer={streamer} />
+    </Box>
   </Main>
 }
