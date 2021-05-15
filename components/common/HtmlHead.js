fff19cba2c5ee168e608b32d72702c8731603ddc
burno <bruno.wieczorek@gmail.com>
2021-05-15 13:00:45 +0200

    Add env file to gitignore, fix crossOrigin font parameter

--- components/common/HtmlHead.js
+++ components/common/HtmlHead.js
@@ -4,6 +4,6 @@ export default function HtmlHead() {
   return <Head>
     <title>twichess</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
-    <link rel="preload" href="https://lichess1.org/assets/_0dhxZ8/font/lichess.woff2" as="font" type="font/woff2" crossorigin></link>
+    <link rel="preload" href="https://lichess1.org/assets/_0dhxZ8/font/lichess.woff2" as="font" type="font/woff2" crossOrigin="true"></link>
   </Head>
 }
